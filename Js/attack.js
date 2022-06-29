e9d66058ca318456a9de048e3ce35e0e9d218e4c
Fallanagel <havemedance@hotmail.com>
2022-06-29 17:54:34 -0500

    attack fuchtion

--- Js/attack.js
+++ Js/attack.js
@@ -1,12 +1,34 @@
 //attack funchion for player and npc
-
-const Attack = (attacker, target, damage) =>{
-   this.attacker = attacker;
-   this.target = target;
-   this.dmg = damage;
- 
+const playerAtk = {
+      health: 100,
+      Atk: 5,
+      Def: 3,
+   }
    
-   
-return attk 
+const npcAtk = {
+   health: 100,
+   Atk: 4,
+   Def: 2,
 }
+//player attack
+const playerAttack = (player, target,) =>{
+let atkSum = player.Atk - npcAtk.Def,
+lossingHealth= npcAtk.health - atkSum
+if (npcAtk.health < 1){
+   alert('You win')
+}
+ }
+
+ //npc attack
+ const npcattack = (npc, target) => {
+   let atkSum = npcAtk.Atk - playerAtk.Def,
+   lossingHealth= playerAtk.health - atkSum
+   if (playerAtk.health < 1){
+      alert('You lose')
+   }
+ } 
+   
+   
+
+
 
