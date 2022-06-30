e29ffb569e94cb43a2b52a06a7d9e612ef6b5c71
Fallanagel <havemedance@hotmail.com>
2022-06-29 21:15:39 -0500

    moved fuchions to diff files and tried to get a map on screen

--- Js/move.js
+++ Js/move.js
@@ -36,19 +36,6 @@ const moveNPCchart = (x, y) => {
     
     let direction = null;
 
-    function moveCharacter() {
-        if (direction === 'west') {
-            x -= 1
-        }
-        if (direction === 'north') {
-            y += 1
-        }
-        if (direction === 'east') {
-            x += 1
-        }
-        if (direction === 'south') {
-            y -= 1
-        }
         element.style.left = x + 'px'
         element.style.bottom = y + 'px'
     }
